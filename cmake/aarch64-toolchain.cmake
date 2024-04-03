99d872dc15c07c67fb7c9c0701ee7e9b38a1dcc3
Neil Hanlon <neil@shrug.pw>
2024-04-02 22:02:56 -0400

    debug

---
+++ cmake/aarch64-toolchain.cmake
@@ -0,0 +1,7 @@
+# aarch64-toolchain.cmake
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
+
+# Specify the cross compilers
+set(CMAKE_C_COMPILER aarch64-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER aarch64-linux-gnu-g++)
